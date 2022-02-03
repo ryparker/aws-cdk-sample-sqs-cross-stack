562449b4bac03575af274e8bef4dde33bb7cadf0
Ryan Parker <ryan.parker3@outlook.com>
2022-02-03 15:54:33 -0800

    SIM D38332497

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,27 @@
-import { App, Stack } from 'aws-cdk-lib';
+import { App, Stack } from "aws-cdk-lib";
+import { Queue } from "aws-cdk-lib/aws-sqs";
+import { EventBus, Rule } from "aws-cdk-lib/aws-events";
+import { SqsQueue } from "aws-cdk-lib/aws-events-targets";
 
 const app = new App();
-const stack = new Stack(app, 'sample');
+
+// Stack A
+const queueProducerStack = new Stack(app, "QueueProducer");
+const mainQueue = new Queue(queueProducerStack, "MainQueue");
+
+// Stack B
+const queueConsumerStack = new Stack(app, "QueueConsumer");
+const eventBus = new EventBus(queueConsumerStack, "EventBus");
+const eventRule = new Rule(queueConsumerStack, "MyRule", {
+  eventBus,
+  eventPattern: {
+    account: [Stack.of(queueConsumerStack).account],
+  },
+});
+const mainQueueRef = Queue.fromQueueArn(
+  queueConsumerStack,
+  "MainQueueRef",
+  mainQueue.queueArn
+);
+const sqsQueue = new SqsQueue(mainQueueRef);
+eventRule.addTarget(sqsQueue);
